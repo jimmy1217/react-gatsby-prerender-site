8c8a343aa72d091de1a6431b55fcbd7c3d712ac7
jimmy <jimmy741217@gmail.com>
2018-02-03 02:10:42 +0800

    update config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,8 @@
 module.exports = {
+  proxy: {
+    prefix: "/api", 
+    url: "http://dev.example.com",  //哪天要打從localhost 打api 的時候改這段
+  },
   pathPrefix: "/react-gatsby-prerender-site",
   siteMetadata: {
     title: 'Gatsby Default Starter',
--- gatsby-node.js
+++ gatsby-node.js
@@ -1,11 +1,29 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const CompressionPlugin = require('compression-webpack-plugin');
+exports.modifyWebpackConfig = ({ config, stage }) => {
+  switch (stage) {
+    case "develop":
+      break;
 
-// You can delete this file if you're not using it
+    case "build-css":
+      break;
 
-exports.modifyWebpackConfig = function(config, env) {
+    case "build-html":
+      break;
+
+    case "build-javascript":
+      // config.loader('js', (current) => {
+      //   console.log(JSON.stringify(current, null, 2));
+      //   return current;
+      // });
+      config.plugin("CompressionPlugin", CompressionPlugin, [{
+        asset: "[path].gz[query]",
+        algorithm: "gzip",
+        test: /\.js$|\.css$|\.html$/,
+        threshold: 10240,
+        minRatio: 0.7
+      }]);
+      break;
+  }
+
   return config;
-}
+};
